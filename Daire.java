b19f7fd524368e24affaa4a466d13073bea4eb6d
Vuslat Sena <47564075+VuslatSena@users.noreply.github.com>
2020-03-31 10:26:23 +0300

    Add files via upload

---
+++ Daire.java
@@ -0,0 +1,14 @@
+public class Daire extends Sekil {
+
+    private int yaricap;
+    public Daire(String isim, int yericap){
+        super(isim);
+        this.yaricap=yaricap;
+    }
+
+    @Override
+   public void alanHesapla() {
+        System.out.println(getIsim()+ "in alanı: "+ (Math.PI* yaricap*yaricap));
+
+    }
+}
